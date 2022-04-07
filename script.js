af28ff146b00adf541ddf7a088f3fd02cb0b091d
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-04-07 07:30:29 -0400

    Started to add funcatiality to save to loacal storage

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
   window.setInterval(function () {
     var today = moment();
-    // $("#1a").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    $("#1a").text(today.format("h"));
+    $("#1a").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+ 
     
     }, 1000);
 
@@ -13,51 +13,139 @@
       
     
     /* -------------------------------------------------------------------------- */
-    document.getElementById("hour-eleven").style.backgroundColor = "blue";
-
+ 
+backround()
     // 1.function past
     function backround() {
       // hour of the day 
       var today = moment();
       var hour = today.format("h");
+      
+
+
+
+
 
       // conditionals to descide if it is past present or future 
       switch (hour) {
         case 9:
-          document.getElementsByClassName("information").style.background = "blue";
+          document.getElementById("hour-nine").style.backgroundColor = "green";
+          document.getElementById("hour-ten").style.backgroundColor = "red";
+          document.getElementById("hour-eleven").style.backgroundColor = "red";
+          document.getElementById("hour-tweleve").style.backgroundColor = "red";
+          document.getElementById("hour-thirteen").style.backgroundColor = "red";
+          document.getElementById("hour-fourteen").style.backgroundColor = "red";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "red";
+          document.getElementById("hour-sixteen").style.backgroundColor = "red";
+          document.getElementById("hour-seventeen").style.backgroundColor = "red";
+        
+          
           
           break;
         case 10:
+          document.getElementById("hour-nine").style.backgroundColor = "yellow";
+          document.getElementById("hour-ten").style.backgroundColor = "green";
+          document.getElementById("hour-eleven").style.backgroundColor = "red";
+          document.getElementById("hour-tweleve").style.backgroundColor = "red";
+          document.getElementById("hour-thirteen").style.backgroundColor = "red";
+          document.getElementById("hour-fourteen").style.backgroundColor = "red";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "red";
+          document.getElementById("hour-sixteen").style.backgroundColor = "red";
+          document.getElementById("hour-seventeen").style.backgroundColor = "red";
           
           break;
         case 11:
+          document.getElementById("hour-nine").style.backgroundColor = "yellow";
+          document.getElementById("hour-ten").style.backgroundColor = "yellow";
+          document.getElementById("hour-eleven").style.backgroundColor = "green";
+          document.getElementById("hour-tweleve").style.backgroundColor = "red";
+          document.getElementById("hour-thirteen").style.backgroundColor = "red";
+          document.getElementById("hour-fourteen").style.backgroundColor = "red";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "red";
+          document.getElementById("hour-sixteen").style.backgroundColor = "red";
+          document.getElementById("hour-seventeen").style.backgroundColor = "red";
           
           break;
 
         case 12:
+          document.getElementById("hour-nine").style.backgroundColor = "yellow";
+          document.getElementById("hour-ten").style.backgroundColor = "yellow";
+          document.getElementById("hour-eleven").style.backgroundColor = "yellow";
+          document.getElementById("hour-tweleve").style.backgroundColor = "green";
+          document.getElementById("hour-thirteen").style.backgroundColor = "red";
+          document.getElementById("hour-fourteen").style.backgroundColor = "red";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "red";
+          document.getElementById("hour-sixteen").style.backgroundColor = "red";
+          document.getElementById("hour-seventeen").style.backgroundColor = "red";
           
           break;
 
         case 1:
+          document.getElementById("hour-nine").style.backgroundColor = "yellow";
+          document.getElementById("hour-ten").style.backgroundColor = "yellow";
+          document.getElementById("hour-eleven").style.backgroundColor = "yellow";
+          document.getElementById("hour-tweleve").style.backgroundColor = "yellow";
+          document.getElementById("hour-thirteen").style.backgroundColor = "green";
+          document.getElementById("hour-fourteen").style.backgroundColor = "red";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "red";
+          document.getElementById("hour-sixteen").style.backgroundColor = "red";
+          document.getElementById("hour-seventeen").style.backgroundColor = "red";
           
           break;
 
         case 2:
+          document.getElementById("hour-nine").style.backgroundColor = "yellow";
+          document.getElementById("hour-ten").style.backgroundColor = "yellow";
+          document.getElementById("hour-eleven").style.backgroundColor = "yellow";
+          document.getElementById("hour-tweleve").style.backgroundColor = "yellow";
+          document.getElementById("hour-thirteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-fourteen").style.backgroundColor = "green";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "red";
+          document.getElementById("hour-sixteen").style.backgroundColor = "red";
+          document.getElementById("hour-seventeen").style.backgroundColor = "red";
           
           break;
         case 3:
+          document.getElementById("hour-nine").style.backgroundColor = "yellow";
+          document.getElementById("hour-ten").style.backgroundColor = "yellow";
+          document.getElementById("hour-eleven").style.backgroundColor = "yellow";
+          document.getElementById("hour-tweleve").style.backgroundColor = "yellow";
+          document.getElementById("hour-thirteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-fourteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "green";
+          document.getElementById("hour-sixteen").style.backgroundColor = "red";
+          document.getElementById("hour-seventeen").style.backgroundColor = "red";
           
           break;
         case 4:
+          document.getElementById("hour-nine").style.backgroundColor = "yellow";
+          document.getElementById("hour-ten").style.backgroundColor = "yellow";
+          document.getElementById("hour-eleven").style.backgroundColor = "yellow";
+          document.getElementById("hour-tweleve").style.backgroundColor = "yellow";
+          document.getElementById("hour-thirteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-fourteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-sixteen").style.backgroundColor = "green";
+          document.getElementById("hour-seventeen").style.backgroundColor = "red";
           
           break;
 
         case 5:
+          document.getElementById("hour-nine").style.backgroundColor = "yellow";
+          document.getElementById("hour-ten").style.backgroundColor = "yellow";
+          document.getElementById("hour-eleven").style.backgroundColor = "yellow";
+          document.getElementById("hour-tweleve").style.backgroundColor = "yellow";
+          document.getElementById("hour-thirteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-fourteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-fifthteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-sixteen").style.backgroundColor = "yellow";
+          document.getElementById("hour-seventeen").style.backgroundColor = "green";
           
           
           break;
       
         default:
+
           break;
       }
 
@@ -66,20 +154,48 @@
 
     };
 
-    function past() {
+   /* --------------------------- save text to button -------------------------- */
+
+   var shoppingFormEl = $('#saveBTN');
+   var shoppingListEl = $('#info');
+function change(){
+   document.getElementById("hour-seventeen").style.backgroundColor = "green";
+   var textInTxtArea = $('textarea[name="log"]').val();
+   localStorage.setItem("textInTxtArea", textInTxtArea);
+
+   console.log(textInTxtArea);
+
+};
 
 
+shoppingFormEl.on('click', change);
 
-    };
+var text = localStorage.getItem("textInTxtArea");
+var area = $('textarea[name="log"]');
 
-    // 2.function present 
-
-    function present() {
-
-    };
-
-    // 3.function future 
-
-    function future() {
-
-    };
+text.append(area);
+   
+   // create function to handle form submission
+   function handleFormSubmit(event) {
+     event.preventDefault();
+   
+     // select form element by its `name` attribute and get its value
+     var shoppingItem = $('textarea[name="log"]').val();
+   
+     // if there's nothing in the form entered, don't print to the page
+     if (!shoppingItem) {
+       console.log('No shopping item filled out in form!');
+       return;
+     }
+   
+     // print to the page
+     localStorage.setItem("shoppingItem", shoppingItem);
+    
+     shoppingListEl.append('<li>' + shoppingItem + '</li>');
+   
+     // clear the form input element
+     $('input[name="shopping-input"]').val('');
+   }
+   
+   // Create a submit event listener on the form element
+  
