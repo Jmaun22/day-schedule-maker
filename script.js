b3e0f94a5ec4d3e51964e540109ca888835342a1
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-04-07 16:54:39 -0400

    Added the save function

--- script.js
+++ script.js
@@ -13,13 +13,20 @@
       
     
     /* -------------------------------------------------------------------------- */
- 
-backround()
+    var today = moment();
+    var hour = today.format("h");
+    var hourI = Number.parseInt(hour)
+    console.log(typeof(hourI))
+backround(hourI);
     // 1.function past
-    function backround() {
+    function backround(hour) {
       // hour of the day 
-      var today = moment();
-      var hour = today.format("h");
+      // var today = moment();
+      // var hour = today.format("h");
+      console.log("plz " + hour)
+
+    
+      
       
 
 
@@ -153,49 +160,166 @@ backround()
       $("#1a").text(hour);
 
     };
-
+    /* -------------------------------------------------------------------------- */
+    
    /* --------------------------- save text to button -------------------------- */
 
    var shoppingFormEl = $('#saveBTN');
-   var shoppingListEl = $('#info');
+   var shoppingFormElB = $('#secondBT');
+
+   var shoppingFormElC = $('#saveBTN2');
+   var shoppingFormElD = $('#saveBTN3');
+   var shoppingFormElE = $('#saveBTN4');
+   var shoppingFormElF = $('#saveBTN5');
+   var shoppingFormElG = $('#saveBTN6');
+   var shoppingFormElH = $('#saveBTN7');
+   var shoppingFormElI = $('#saveBTN8');
+
+
+   
 function change(){
-   document.getElementById("hour-seventeen").style.backgroundColor = "green";
+  
    var textInTxtArea = $('textarea[name="log"]').val();
    localStorage.setItem("textInTxtArea", textInTxtArea);
 
    console.log(textInTxtArea);
 
+   var textInTxtAreaB = $('textarea[name="logB"]').val();
+   localStorage.setItem("textInTxtAreaB", textInTxtAreaB);
+
+   var textInTxtAreaC = $('textarea[name="log2"]').val();
+   localStorage.setItem("textInTxtAreaC", textInTxtAreaC);
+
+   var textInTxtAreaD = $('textarea[name="log2"]').val();
+   localStorage.setItem("textInTxtAreaD", textInTxtAreaD);
+
+   var textInTxtAreaE = $('textarea[name="log3"]').val();
+   localStorage.setItem("textInTxtAreaE", textInTxtAreaE);
+
+   var textInTxtAreaF = $('textarea[name="log4"]').val();
+   localStorage.setItem("textInTxtAreaF", textInTxtAreaF);
+
+   var textInTxtAreaG = $('textarea[name="log5"]').val();
+   localStorage.setItem("textInTxtAreaG", textInTxtAreaG);
+
+   var textInTxtAreaH = $('textarea[name="log6"]').val();
+   localStorage.setItem("textInTxtAreaH", textInTxtAreaH);
+
+   var textInTxtAreaI = $('textarea[name="log7"]').val();
+   localStorage.setItem("textInTxtAreaI", textInTxtAreaI);
+
+   var textInTxtAreaJ = $('textarea[name="log8"]').val();
+   localStorage.setItem("textInTxtAreaJ", textInTxtAreaJ);
+   
+   
+
+
 };
 
 
-shoppingFormEl.on('click', change);
 
+shoppingFormEl.on('click', change);
+shoppingFormElB.on('click', change);
+shoppingFormElC.on('click', change);
+shoppingFormElD.on('click', change);
+shoppingFormElE.on('click', change);
+
+shoppingFormElF.on('click', change);
+shoppingFormElG.on('click', change);
+shoppingFormElH.on('click', change);
+shoppingFormElI.on('click', change);
+
+
+/* -------------------------------------------------------------------------- */
+
+// 9am
 var text = localStorage.getItem("textInTxtArea");
+
+var textB = localStorage.getItem("textInTxtAreaB");
+var textC = localStorage.getItem("textInTxtAreaC");
+var textD = localStorage.getItem("textInTxtAreaD");
+var textE = localStorage.getItem("textInTxtAreaE");
+var textF = localStorage.getItem("textInTxtAreaF");
+var textG = localStorage.getItem("textInTxtAreaG");
+var textH = localStorage.getItem("textInTxtAreaH");
+var textI = localStorage.getItem("textInTxtAreaI");
+
+
 var area = $('textarea[name="log"]');
 
-text.append(area);
+var areaB = $('textarea[name="logB"]');
+var areaC = $('textarea[name="logC"]');
+var areaD = $('textarea[name="logD"]');
+var areaE = $('textarea[name="logE"]');
+var areaF = $('textarea[name="logF"]');
+var areaG = $('textarea[name="logG"]');
+var areaH = $('textarea[name="logH"]');
+var areaI = $('textarea[name="logI"]');
+
+console.log("yo" + text);
+area.textContent = text;
+
+areaB.textContent = textB;
+areaC.textContent = textC;
+areaD.textContent = textD;
+areaE.textContent = textE;
+areaF.textContent = textF;
+areaG.textContent = textG;
+areaH.textContent = textH;
+areaI.textContent = textI;
+
+
+area.append(text);
+
+areaB.append(textB);
+areaC.append(textC);
+areaD.append(textD);
+areaE.append(textE);
+areaF.append(textF);
+areaG.append(textG);
+areaH.append(textH);
+areaI.append(textI);
+
+text.append('<p>' + area + '</p>');
+
+textB.append('<p>' + areaB + '</p>');
+textC.append('<p>' + areaC + '</p>');
+textD.append('<p>' + areaD + '</p>');
+textE.append('<p>' + areaE + '</p>');
+textF.append('<p>' + areaF + '</p>');
+textG.append('<p>' + areaG + '</p>');
+textH.append('<p>' + areaH + '</p>');
+textI.append('<p>' + areaI + '</p>');
+
+// 10am
+
    
-   // create function to handle form submission
-   function handleFormSubmit(event) {
-     event.preventDefault();
+
+
+
+
+
+//11am
+
+
+// 12am
+
+
+// 1pm
+
+
+// 2pm
+
+// 3pm
+
+
+// 4pm
+
+
+// 5pm
+
+
+
    
-     // select form element by its `name` attribute and get its value
-     var shoppingItem = $('textarea[name="log"]').val();
-   
-     // if there's nothing in the form entered, don't print to the page
-     if (!shoppingItem) {
-       console.log('No shopping item filled out in form!');
-       return;
-     }
-   
-     // print to the page
-     localStorage.setItem("shoppingItem", shoppingItem);
-    
-     shoppingListEl.append('<li>' + shoppingItem + '</li>');
-   
-     // clear the form input element
-     $('input[name="shopping-input"]').val('');
-   }
-   
-   // Create a submit event listener on the form element
+
   
